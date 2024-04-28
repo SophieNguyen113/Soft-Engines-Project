4c51476a2c9f8e836f24bac7b5e2ce1a64b17c81
SophieNguyen113 <sophiengocnguyen113@gmail.com>
2024-04-28 13:38:07 -0400

    Added data for SSN

--- Soft_Engines/src/employee/AddSSNColumn.java
+++ Soft_Engines/src/employee/AddSSNColumn.java
@@ -1,6 +1,7 @@
 package employee;
 
 import java.sql.*;
+import java.util.Random;
 
 public class AddSSNColumn {
     public static void addSSNColumn(Connection myConn) {
@@ -12,10 +13,25 @@ public class AddSSNColumn {
                 myStmt.executeUpdate("ALTER TABLE employees DROP COLUMN SSN");
                 System.out.println("Existing SSN column dropped.");
             }
-            
+
             String sqlCommand = "ALTER TABLE employees ADD COLUMN SSN INT NOT NULL;";
             myStmt.executeUpdate(sqlCommand);
-            System.out.println("Report 4: SSN column added to the employees table successfully.");
+            System.out.println("Report 4: SSN column added to the Employees table successfully.");
+
+            Random random = new Random();
+            String updateQuery = "UPDATE employees SET SSN = ? WHERE empid = ?";
+            PreparedStatement pstmt = myConn.prepareStatement(updateQuery);
+
+            ResultSet employeesRS = myStmt.executeQuery("SELECT empid FROM employees");
+            while (employeesRS.next()) {
+                int empid = employeesRS.getInt("empid");
+                int fakeSSN = random.nextInt(900000000) + 100000000; 
+                pstmt.setInt(1, fakeSSN);
+                pstmt.setInt(2, empid);
+                pstmt.executeUpdate();
+            }
+
+            System.out.println("SSN data inserted successfully.");
         } catch (Exception e) {
             System.out.println("ERROR " + e.getLocalizedMessage());
         }
